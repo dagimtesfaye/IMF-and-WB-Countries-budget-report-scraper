52af2e3da6461b9ecc21e9eba1736333d51375e4
dagimtesfaye <106874096+dagimtesfaye@users.noreply.github.com>
2025-08-04 23:52:59 +0300

    Extracts the official name from the infobox

--- gdp_scraper/gdp_scraper/spiders/crawler.py
+++ gdp_scraper/gdp_scraper/spiders/crawler.py
@@ -4,15 +4,17 @@ import scrapy
 class CrawlerSpider(scrapy.Spider):
     name = "crawler"
     allowed_domains = ["en.wikipedia.org"]
-
     start_urls = ["https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)"]
 
     def parse(self, response):
-        
-        main_path = response.xpath('(//table[contains(@class, "wikitable")]/tbody/tr)[position() > 1]')
-        for row in main_path:
-            yield {
-                'country': row.xpath('./td[1]/a/text()').get(),
+        rows = response.xpath('(//table[contains(@class, "wikitable")]/tbody/tr)[position() > 1]')
+
+        for row in rows:
+            country_name = row.xpath('./td[1]/a/text()').get()
+            country_link = row.xpath('./td[1]/a/@href').get()
+
+            item = {
+                'country': country_name,
                 'IMF-estimated-GDP': row.xpath('./td[2]/text()').get(),
                 'IMF-year': row.xpath('./td[3]/text()').get(),
                 'WB-estimated-GDP': row.xpath('./td[4]/text()').get(),
@@ -20,3 +22,20 @@ class CrawlerSpider(scrapy.Spider):
                 'UN-estimated-GDP': row.xpath('./td[6]/text()').get(),
                 'UN-year': row.xpath('./td[7]/text()').get(),
             }
+
+            if country_link:
+                full_url = response.urljoin(country_link)
+                yield response.follow(full_url, callback=self.parse_country_page, meta={'item': item})
+            else:
+                yield item
+
+    def parse_country_page(self, response):
+        item = response.meta['item']
+        # Try to get the official name from the infobox (common format)
+        official_name = response.xpath('//table[contains(@class, "infobox")]//th[contains(text(), "Official")]/following-sibling::td//text()').get()
+        
+        if official_name:
+            official_name = official_name.strip()
+
+        item['official_name'] = official_name or "N/A"
+        yield item
